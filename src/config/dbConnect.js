23ddf8aedb98a413e12ec4e01ba8d23e8d371c71
GabrielPresense <gabrielpresense@gmail.com>
2024-05-04 04:57:47 -0300

    Acessando a coleção livros

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,9 +1,9 @@
 import mongoose, { mongo } from "mongoose";
 
 async function conectaNaDatabase(){
-    mongoose.connect("mongodb+srv://admin:admin123@cluster0.v202khc.mongodb.net/livraria?retryWrites=true&w=majority&appName=Cluster0");
+    mongoose.connect(process.env.DB_CONNECTION_STRING);
 
     return mongoose.connection;
-}
+};
 
 export default conectaNaDatabase;
