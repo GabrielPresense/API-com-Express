232ff2ad1a881aec8e0769dc9f58d3da3f94a00a
GabrielPresense <gabrielpresense@gmail.com>
2024-05-04 09:43:36 -0300

    Conclusão

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -53,6 +53,16 @@ class LivroController {
             res.status(500).json({ message: `${erro.message} - falha na exclusão` });
         }
     };
+
+    static async listarLivrosPorEditora (req, res) {
+        const editora = req.query.editora;
+        try {
+            const listarLivrosPorEditora = await livro.find({ editora: editora });
+            res.status(200).json(listarLivrosPorEditora); 
+        } catch (erro) {
+            res.status(500).json({ message: `${erro.message} - falha na busca` });
+        }
+    }
 };
 
 export default LivroController;
